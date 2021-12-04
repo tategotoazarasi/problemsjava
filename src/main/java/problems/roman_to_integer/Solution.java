bf92e61a2efea2c0d4f46f83fe46e78577891af9
Tategoto Azarasi <2724167997@qq.com>
2021-12-04 10:33:10 +0800

    roman to integer

---
+++ src/main/java/problems/roman_to_integer/Solution.java
@@ -0,0 +1,55 @@
+package problems.roman_to_integer;
+
+public class Solution {
+    public int romanToInt(String s) {
+        int[] vec = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case 'I': {
+                    vec[i] = 1;
+                    break;
+                }
+                case 'V': {
+                    vec[i] = 5;
+                    break;
+                }
+                case 'X': {
+                    vec[i] = 10;
+                    break;
+                }
+                case 'L': {
+                    vec[i] = 50;
+                    break;
+                }
+                case 'C': {
+                    vec[i] = 100;
+                    break;
+                }
+                case 'D': {
+                    vec[i] = 500;
+                    break;
+                }
+                case 'M': {
+                    vec[i] = 1000;
+                    break;
+                }
+            }
+        }
+        int sum = 0;
+        for (int i = vec.length - 1; i > 0; i--) {
+            if ((vec[i] == 5 || vec[i] == 10) && vec[i - 1] == 1) {
+                vec[i] -= 1;
+                vec[i - 1] = 0;
+            } else if ((vec[i] == 50 || vec[i] == 100) && vec[i - 1] == 10) {
+                vec[i] -= 10;
+                vec[i - 1] = 0;
+            } else if ((vec[i] == 500 || vec[i] == 1000) && vec[i - 1] == 100) {
+                vec[i] -= 100;
+                vec[i - 1] = 0;
+            }
+            sum += vec[i];
+        }
+        sum += vec[0];
+        return sum;
+    }
+}
---
+++ src/test/java/problems/roman_to_integer/SolutionTest.java
@@ -0,0 +1,18 @@
+package problems.roman_to_integer;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest extends TestCase {
+
+    @Test
+    void romanToInt() {
+        Solution sol = new Solution();
+        Assertions.assertEquals(3, sol.romanToInt("III"));
+        Assertions.assertEquals(4, sol.romanToInt("IV"));
+        Assertions.assertEquals(9, sol.romanToInt("IX"));
+        Assertions.assertEquals(58, sol.romanToInt("LVIII"));
+        Assertions.assertEquals(1994, sol.romanToInt("MCMXCIV"));
+    }
+}
