328a05f1dd27ffdfd7c61d9a2fff2fa2ba4e74db
Tategoto Azarasi <2724167997@qq.com>
2021-12-08 10:12:05 +0800

    maximum subarray
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

---
+++ src/test/java/problems/maximum_subarray/SolutionTest.java
@@ -0,0 +1,17 @@
+package problems.maximum_subarray;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    @Test
+    void maxSubArray() {
+        Solution sol = new Solution();
+        assertEquals(6, sol.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        assertEquals(1, sol.maxSubArray(new int[]{1}));
+        assertEquals(23, sol.maxSubArray(new int[]{5, 4, -1, 7, 8}));
+        assertEquals(-1, sol.maxSubArray(new int[]{-5, -4, -1, -7, -8}));
+    }
+}
