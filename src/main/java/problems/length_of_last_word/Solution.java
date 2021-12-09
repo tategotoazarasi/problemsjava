5911a1c71681331b5bdf19a3b51995b1006b70b1
Tategoto Azarasi <2724167997@qq.com>
2021-12-09 10:25:07 +0800

    maximum subarray
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

---
+++ src/main/java/problems/length_of_last_word/Solution.java
@@ -0,0 +1,26 @@
+package problems.length_of_last_word;
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        boolean flag1 = true;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (flag1) {
+                if (s.charAt(i) == ' ') {
+                    continue;
+                } else {
+                    flag1 = false;
+                    length++;
+                    continue;
+                }
+            }
+
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else {
+                break;
+            }
+        }
+        return length;
+    }
+}
