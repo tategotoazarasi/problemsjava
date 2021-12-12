68f96713cc039817569a05f1ae13cb2a121c39e3
Tategoto Azarasi <2724167997@qq.com>
2021-12-12 17:09:13 +0800

    remove duplicates from sorted list
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

---
+++ src/main/java/problems/remove_duplicates_from_sorted_list/ListNode.java
@@ -0,0 +1,18 @@
+package problems.remove_duplicates_from_sorted_list;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
