e3a5e6789b2d85d5d5f51a1c9de1fd2074a849dc
Tategoto Azarasi <2724167997@qq.com>
2021-12-13 10:07:02 +0800

    merge sorted array
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

---
+++ src/main/java/leetcode/merge_sorted_array/Solution.java
@@ -0,0 +1,32 @@
+package leetcode.merge_sorted_array;
+
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0) {
+            return;
+        }
+        if (m == 0) {
+            System.arraycopy(nums2, 0, nums1, 0, m + n);
+            return;
+        }
+        int[] ret = new int[m + n];
+        int i = 0, j = 0, k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                ret[k++] = nums1[i++];
+            } else {
+                ret[k++] = nums2[j++];
+            }
+        }
+        if (i == m) {
+            while (j < n) {
+                ret[k++] = nums2[j++];
+            }
+        } else {
+            while (i < m) {
+                ret[k++] = nums1[i++];
+            }
+        }
+        System.arraycopy(ret, 0, nums1, 0, m + n);
+    }
+}
