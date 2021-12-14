746ec18fcd11efac50378b68b34f356c5ced3805
Tategoto Azarasi <2724167997@qq.com>
2021-12-14 10:23:13 +0800

    symmetric tree
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

---
+++ src/test/java/leetcode/symmetric_tree/SolutionTest.java
@@ -0,0 +1,16 @@
+package leetcode.symmetric_tree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SolutionTest {
+
+    @Test
+    void isSymmetric() {
+        Solution sol = new Solution();
+        assertTrue(sol.isSymmetric(new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)))));
+        assertFalse(sol.isSymmetric(new TreeNode(1, new TreeNode(2, null, new TreeNode(3)), new TreeNode(2, null, new TreeNode(3)))));
+    }
+}
