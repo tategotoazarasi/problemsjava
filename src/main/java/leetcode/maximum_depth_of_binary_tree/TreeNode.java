9e12aaf453e14e8514c9d4b1c7aa94e485caa165
Tategoto Azarasi <2724167997@qq.com>
2021-12-15 09:51:33 +0800

    maximum depth of binary tree
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

---
+++ src/main/java/leetcode/maximum_depth_of_binary_tree/TreeNode.java
@@ -0,0 +1,20 @@
+package leetcode.maximum_depth_of_binary_tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ src/test/java/leetcode/maximum_depth_of_binary_tree/SolutionTest.java
@@ -0,0 +1,14 @@
+package leetcode.maximum_depth_of_binary_tree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    @Test
+    void maxDepth() {
+        Solution sol = new Solution();
+        assertEquals(3, sol.maxDepth(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))));
+    }
+}
