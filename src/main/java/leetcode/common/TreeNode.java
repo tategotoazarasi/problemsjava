a032a9069f58bc87cf1e2809f07c5b5b556f326b
Tategoto Azarasi <2724167997@qq.com>
2021-12-16 10:01:21 +0800

    balanced binary tree
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- src/main/java/leetcode/common/TreeNode.java
+++ src/main/java/leetcode/common/TreeNode.java
@@ -1,18 +1,18 @@
 package leetcode.common;
 
 public class TreeNode {
-    int val;
-    leetcode.convert_sorted_array_to_binary_search_tree.TreeNode left;
-    leetcode.convert_sorted_array_to_binary_search_tree.TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
-    TreeNode() {
+    public TreeNode() {
     }
 
-    TreeNode(int val) {
+    public TreeNode(int val) {
         this.val = val;
     }
 
-    TreeNode(int val, leetcode.convert_sorted_array_to_binary_search_tree.TreeNode left, leetcode.convert_sorted_array_to_binary_search_tree.TreeNode right) {
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
