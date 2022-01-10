5f75f376abd87a72e3719c9d423f726e1819054e
Tategoto Azarasi <2724167997@qq.com>
2022-01-10 11:43:19 +0800

    LintCode 2378. final 变量不可变
    Signed-off-by: tategotoazarasi <2724167997@qq.com>

---
+++ src/main/java/lintcode/lintcode2378/Main.java
@@ -0,0 +1,24 @@
+/**
+ * LintCode 2378. final 变量不可变
+ */
+package lintcode.lintcode2378;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+
+public class Main {
+    public static void main(String[] args) throws NoSuchFieldException {
+        Solution solution = new Solution();
+        User uer = solution.getUser(args[0]);
+        Class clazz = Solution.class;
+        Field field = clazz.getDeclaredField("user");
+
+        if (!Modifier.isFinal(field.getModifiers())) {
+            throw new RuntimeException(
+                    "You need to make sure that the variable user is final");
+        }
+
+        System.out.println(uer.getName());
+    }
+}
---
+++ src/main/java/lintcode/lintcode2378/Solution.java
@@ -0,0 +1,14 @@
+package lintcode.lintcode2378;
+
+public class Solution {
+    final User user;
+
+    public Solution() {
+        user = new User();
+    }
+
+    public User getUser(String arg) {
+        user.setName(arg);
+        return user;
+    }
+}
