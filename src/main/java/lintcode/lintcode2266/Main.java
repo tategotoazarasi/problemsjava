d688a03472884210a8530a3faf172752c71b5931
tategotoazarasi <2724167997@qq.com>
2022-01-13 10:25:28 +0800

    LintCode 2266. 交换两个整数（Java 版）
    Signed-off-by: tategotoazarasi <2724167997@qq.com>

---
+++ src/main/java/lintcode/lintcode2266/Main.java
@@ -0,0 +1,17 @@
+package lintcode.lintcode2266;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // write your code here
+        int c = a;
+        a = b;
+        b = c;
+        // keep the code below
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
