d4e8955ba63a6475b0f199971b732735f2ff2981
Tategoto Azarasi <2724167997@qq.com>
2022-12-25 21:26:39 +0800

    218. 天际线问题
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- src/main/java/leetcode/the_skyline_problem/Solution.java
+++ src/main/java/leetcode/the_skyline_problem/Solution.java
@@ -1,36 +1,36 @@
 package leetcode.the_skyline_problem;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
 
 /**
  * 218. 天际线问题
  */
 public class Solution {
 	public List<List<Integer>> getSkyline(int[][] buildings) {
-		PriorityQueue<int[]> pq         = new PriorityQueue<int[]>((a, b) -> b[1] - a[1]);
-		List<Integer>        boundaries = new ArrayList<Integer>();
+		List<List<Integer>>       ans = new ArrayList<>();
+		TreeMap<Integer, Integer> top = new TreeMap<>();
 		for (int[] building : buildings) {
-			boundaries.add(building[0]);
-			boundaries.add(building[1]);
+			top.put(building[0], Math.max(top.getOrDefault(building[0], 0), building[2]));
+			top.put(building[1], top.getOrDefault(building[1], 0));
 		}
-		Collections.sort(boundaries);
-
-		List<List<Integer>> ret = new ArrayList<List<Integer>>();
-		int                 n   = buildings.length, idx = 0;
-		for (int boundary : boundaries) {
-			while (idx < n && buildings[idx][0] <= boundary) {
-				pq.offer(new int[]{buildings[idx][1], buildings[idx][2]});
-				idx++;
-			}
-			while (!pq.isEmpty() && pq.peek()[0] <= boundary) {
-				pq.poll();
-			}
-
-			int maxn = pq.isEmpty() ? 0 : pq.peek()[1];
-			if (ret.size() == 0 || maxn != ret.get(ret.size() - 1).get(1)) {
-				ret.add(Arrays.asList(boundary, maxn));
+		for (int[] building : buildings) {
+			var es = top.subMap(building[0] + 1, building[1]).entrySet();
+			for (var entry : es) {
+				entry.setValue(Math.max(entry.getValue(), building[2]));
 			}
 		}
-		return ret;
+		int prevH = 0;
+		for (var entry : top.entrySet()) {
+			if (entry.getValue() != prevH) {
+				List<Integer> p = new ArrayList<>();
+				p.add(entry.getKey());
+				p.add(entry.getValue());
+				ans.add(p);
+			}
+			prevH = entry.getValue();
+		}
+		return ans;
 	}
 }
